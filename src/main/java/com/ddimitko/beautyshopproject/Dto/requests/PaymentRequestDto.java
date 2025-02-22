4497264c939367c76572f3b77c9eaae4adf0c17d
ddimitko <mitkovencislavov@gmail.com>
2025-02-22 19:39:31 +0200

    Payment system now working

--- src/main/java/com/ddimitko/beautyshopproject/Dto/requests/PaymentRequestDto.java
+++ src/main/java/com/ddimitko/beautyshopproject/Dto/requests/PaymentRequestDto.java
@@ -1,5 +1,6 @@
 package com.ddimitko.beautyshopproject.Dto.requests;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,9 +8,14 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class PaymentRequestDto {
 
-    private String method; // "cash" or "card"
-    private String paymentMethodId; // Required if method is "card"
+    private String sessionToken;
+    private Long shopId;
+    private Long customerId;
+    private String customerName;
+    private String customerEmail;
+    private Integer serviceId;
 
 }
